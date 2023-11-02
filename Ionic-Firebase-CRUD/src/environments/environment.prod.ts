18f41d2180b54d73773f57b56a1d2d12900a25d0
JorHaines <jordan.w.haines@gmail.com>
2023-11-02 14:56:47 -0700

    try it this way

---
+++ Ionic-Firebase-CRUD/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyDP3gPbCBErOJx8r1VX_cYF3SNRH9gSUi8",
+      authDomain: "ionicfirebase-6589a.firebaseapp.com",
+      projectId: "ionicfirebase-6589a",
+      storageBucket: "ionicfirebase-6589a.appspot.com",
+      messagingSenderId: "239036929262",
+      appId: "1:239036929262:web:fe94bc3cc26396d386cfff"
+  },
+};
